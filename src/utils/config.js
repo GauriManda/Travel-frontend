9dc0817a84dd89eab8063a04fb42f6a629143f44
Edusan Foundation <edusan.org.in>
2025-06-16 11:17:39 +0530

    changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,2 @@
 // config.js - UPDATE THIS FILE
-export const BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  "https://travel-backend-ashen-nine.vercel.app/api/v1";
+export const BASE_URL = "https://travel-backend-ashen-nine.vercel.app/api/v1";
